71d9db3295016236ec678e3ea86af0f5b46bde51
Hadi <hrumjahn@nevada.unr.edu>
2017-05-05 12:54:16 -0700

    Testing sound complete

--- src/UiMgr.cpp
+++ src/UiMgr.cpp
@@ -53,6 +53,14 @@ void UiMgr::tick(float dt){
 		{
 			engine->theState = STATE::MAIN_MENU;
 			engine->gfxMgr->loadMenu();
+
+			// LOAD MAIN MENU SOUND
+			engine->soundMgr->load_song("Layer 1", "/home/hrumjahn/workspace/Cries/resources/theme.wav");
+			//load_sound(std::string soundName, std::string filePath);
+
+			//play_sound(std::string soundName);
+			engine->soundMgr->play_song("Layer 1", true);
+
 			loadMenu();//Creates the button
 		}
 	} else if(engine->theState == STATE::GAMEPLAY)
